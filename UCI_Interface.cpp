25b39a6866c367adb4e6e6c9379ed54921eebc90
m3003040-create <m3003040@gmail.com>
2026-03-18 21:08:22 +0300

    Update UCI_Interface.cpp

--- UCI_Interface.cpp
+++ UCI_Interface.cpp
@@ -1,95 +1,40 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <sstream>
-#include <iterator>
+#include <vector>
 
-class UCI {
-public:
+// Подключаем связи с другими частями движка
+#include "Bitboard.cpp"
+#include "Position.cpp"
+#include "Search.cpp"
+#include "MoveGen.cpp"
+
+void uci_loop() {
     Position pos;
-    Searcher searcher;
-
-    UCI() : searcher(pos) {}
-
-    void loop() {
-        std::string line, token;
-        while (std::getline(std::cin, line)) {
-            std::istringstream is(line);
-            token.clear();
-            is >> token;
-
-            if (token == "uci") {
-                std::cout << "id name ApexChess_v1" << std::endl;
-                std::cout << "id author Gemini_User" << std::endl;
-                std::cout << "uciok" << std::endl;
-            } else if (token == "isready") {
-                std::cout << "readyok" << std::endl;
-            } else if (token == "ucinewgame") {
-                reset_board(pos.board);
-                init_all_tables();
-            } else if (token == "position") {
-                parse_position(is);
-            } else if (token == "go") {
-                parse_go(is);
-            } else if (token == "quit") {
-                break;
-            } else if (token == "stop") {
-                // Логика прерывания поиска
-            }
+    Searcher searcher(pos); // Теперь он будет знать, что такое searcher
+    reset_board(pos.board);
+    
+    std::string line, command;
+    while (std::getline(std::cin, line)) {
+        std::istringstream is(line);
+        is >> command;
+        if (command == "uci") {
+            std::cout << "id name ApexChess" << std::endl;
+            std::cout << "id author User" << std::endl;
+            std::cout << "uciok" << std::endl;
+        } else if (command == "isready") {
+            std::cout << "readyok" << std::endl;
+        } else if (command == "go") {
+            searcher.iterative_deepening(6); // Запуск поиска
+        } else if (command == "quit") {
+            break;
         }
     }
+}
 
-private:
-    void parse_position(std::istringstream& is) {
-        std::string token, fen;
-        is >> token;
-        if (token == "startpos") {
-            reset_board(pos.board);
-            // Здесь должна быть инициализация начальной расстановки
-            is >> token; 
-        } else if (token == "fen") {
-            while (is >> token && token != "moves") {
-                fen += token + " ";
-            }
-            // Функция загрузки FEN (будет в следующем файле)
-        }
-
-        if (token == "moves") {
-            std::string move_str;
-            while (is >> move_str) {
-                Move m = parse_move(move_str);
-                if (m.data != 0) pos.make_move(m);
-            }
-        }
-    }
-
-    void parse_go(std::istringstream& is) {
-        int depth = 6; // Значение по умолчанию
-        std::string token;
-        while (is >> token) {
-            if (token == "depth") is >> depth;
-        }
-        searcher.iterative_deepening(depth);
-    }
-
-    Move parse_move(std::string str) {
-        MoveList ml;
-        generate_all_moves(pos.board, ml);
-        int from = (str[0] - 'a') + (8 - (str[1] - '0')) * 8;
-        int to = (str[2] - 'a') + (8 - (str[3] - '0')) * 8;
-        
-        for (int i = 0; i < ml.count; i++) {
-            if (ml.moves[i].from() == from && ml.moves[i].to() == to) {
-                return ml.moves[i];
-            }
-        }
-        return Move();
-    }
-};
-
+// Если в этом файле есть main, убедись, что таблицы инициализируются
 int main() {
-    init_all_tables();
-    UCI interface;
-    interface.loop();
+    init_all_tables(); // Теперь эта функция будет видна из Bitboard.cpp
+    uci_loop();
     return 0;
 }
